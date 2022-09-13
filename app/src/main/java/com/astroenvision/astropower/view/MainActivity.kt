8f4e4bf6c29fb4716175f027813a7a3601d9aa40
prahladamigo <singhprahlad366@gmail.com>
2022-09-13 22:02:50 +0530

    Login Module and Splash Screen also all icon added

--- app/src/main/java/com/astroenvision/astropower/view/MainActivity.kt
+++ app/src/main/java/com/astroenvision/astropower/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.astroenvision.astropower
+package com.astroenvision.astropower.view
 
 import android.os.Bundle
 import android.view.Menu
@@ -11,6 +11,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.astroenvision.astropower.R
 import com.astroenvision.astropower.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
