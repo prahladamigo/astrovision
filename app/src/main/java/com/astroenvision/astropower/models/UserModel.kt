2c2423fca18cd4e7c29436e75e27387f0855b3e1
prahladamigo <singhprahlad366@gmail.com>
2022-09-15 16:21:14 +0530

    SignIn and OTP layout

---
+++ app/src/main/java/com/astroenvision/astropower/models/UserModel.kt
@@ -0,0 +1,10 @@
+package com.astroenvision.astropower.models
+
+class UserModel {
+
+    var id = 0
+    var userName: String ? = null
+    var mobileNo: String  ? = null
+    var emailId: String  ? = null
+    var userPhoto: String  ? = null
+}
